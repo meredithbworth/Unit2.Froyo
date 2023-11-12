2352d8c2372bf926d11e058e11ce0835f519d2d2
Meredith Butterworth <meredithbutterworth@gmail.com>
2023-11-12 16:40:43 -0500

    froyoFlavor objects

--- Froyo.js
+++ Froyo.js
@@ -1,2 +1,24 @@
 /* A visitor receives a prompt upon opening the website to enter a list of comma-separated froyo flavors. They type vanilla,vanilla,vanilla,strawberry,coffee,coffee. When they view the browser console, they observe an object listing how many of each flavor they have ordered. In this case, they will be able to observe that they have ordered three vanilla, two coffee, and one strawberry froyo. */
 
+//
+
+const froyoFlavor1 = {
+    name: "Vanilla",
+    color: "white",
+    taste: "generic and warm",
+}
+const froyoFlavor2 = {
+    name: "Coffee",
+    color: "light brown",
+    taste: "earthy and aromatic",
+}
+const froyoFlavor3 = {
+    name: "Strawberry",
+    color: "pink",
+    taste: "fruity and sweet",
+}
+const froyoFlavor4 = {
+    name: "Chocolate",
+    color: "deep brown",
+    taste: "rich and melty",
+}
