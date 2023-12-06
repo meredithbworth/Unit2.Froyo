0507f5fe32918e8e01b3f885623b4225b0548d7a
Meredith Butterworth <meredithbutterworth@gmail.com>
2023-12-06 18:48:27 -0500

    solved! now to repeat...

--- Froyo.js
+++ Froyo.js
@@ -1,72 +1,23 @@
-/* A visitor receives a prompt upon opening the website to enter a list of comma-separated froyo flavors. They type "vanilla,vanilla,vanilla,strawberry,coffee,coffee". When they view the browser console, they observe an object listing how many of each flavor they have ordered. In this case, they will be able to observe that they have ordered three vanilla, two coffee, and one strawberry froyo. */
+/* A visitor receives a prompt upon opening the website to enter a list of comma-separated froyo flavors. They type "vanilla,vanilla,vanilla,strawberry,coffee,coffee". When they view the browser console, they observe an object listing how many of each flavor they have ordered. In this case, they will be able to observe that they have ordered three vanilla, two coffee, and one strawberry froyo.
 
-//
-
-const froyoFlavor = [
-  {
-    name: "vanilla",
-    color: "white",
-    taste: "generic and warm",
-  },
-  {
-    name: "coffee",
-    color: "light brown",
-    taste: "earthy and aromatic",
-  },
-  {
-    name: "strawberry",
-    color: "pink",
-    taste: "fruity and sweet"
-  }
-]
-//console.log(froyoFlavor[1]);
-
-const flavorSum = {
-  totalVanilla: 0,
-  totalCoffee: 0,
-  totalStrawberry: 0
-};
-
-let numbersSum = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    if (userInputString === 'vanilla') {
-      flavorSum.totalVanilla += 1;
-    } else if (userInputString === 'coffee') {
-      flavorSum.totalCoffee += 1;
-    } else if (userInputString === 'strawberry') {
-      flavorSum.totalStrawberry += 1;
-    }
-   
-  } 
-  return numbersSum;
-
-// function iceCream (userInputString) {
-//   if (iceCream === 'vanilla') {
-//     flavorSum.totalVanilla += 1;
-//   } else if (iceCream === 'coffee') {
-//     flavorSum.totalCoffee += 1;
-//   } else if (iceCream === 'strawberry') {
-//     flavorSum.totalStrawberry += 1;
-//   }
-// }
-//   console.log(flavorSum.totalCoffee);
-
-
-// === DO NOT EDIT THIS REGION ===
-// Read the comments to understand how the program is structured.
+We will take in unput from the user of any ice cream flavor in the form of a string. Then we will put those strings of text into an */
 
 const userInputString = prompt(
   "Please enter some Froyo flavors separated by commas.",
   "vanilla, vanilla, vanilla, strawberry, coffee, coffee"
 );
 
-// Split the string of numbers into an array of strings.
-const stringArray = userInputString.split(",");
+// Split the string of ice cream into an array of strings.
+const stringArray = userInputString.split(", ");
+console.log(stringArray);
 
 // Convert the array of strings into an array of numbers.
-const froyoStr = [];
 for (let i = 0; i < stringArray.length; i++) {
   const str = stringArray[i];
-  const number = parseInt(str);
-  numbers.push(number);
+  if (flavorMap[str]) {
+    flavorMap[str]++;
+  } else {
+    flavorMap[str] = 1;
+  }
 }
+console.log(flavorMap);
